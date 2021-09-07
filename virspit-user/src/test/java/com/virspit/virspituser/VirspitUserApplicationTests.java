396259ac8e566d852e98c1e4cdc833ae45f403a8
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-08 02:58:02 +0900

    <feat> add virspit-user

---
+++ virspit-user/src/test/java/com/virspit/virspituser/VirspitUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virspit.virspituser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirspitUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
