816d2084e5c25b9ccde20b91fb2f70c9fcf8dbdb
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-07 11:56:53 +0900

    mkdir

---
+++ virsipit-gateway/src/main/java/com/virsipit/gateway/VirsipitGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.virsipit.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirsipitGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirsipitGatewayApplication.class, args);
+    }
+
+}
