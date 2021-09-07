3aab1f9fd5d322f0f2163602d976c17ac9862c22
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-08 04:45:47 +0900

    <feat> admin 프로젝트 생성

---
+++ virspit-admin/src/main/java/com/virspit/admin/VirspitAdminApplication.java
@@ -0,0 +1,13 @@
+package com.virspit.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirspitAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirspitAdminApplication.class, args);
+	}
+
+}
