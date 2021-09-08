878c1b6c79840998b50d5f895efc3fb1f1f81b13
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-08 18:23:56 +0900

    <feat> auth-server Router 추가

--- virspit-gateway/src/main/java/com/virspit/gateway/VirspitGatewayApplication.java
+++ virspit-gateway/src/main/java/com/virspit/gateway/VirspitGatewayApplication.java
@@ -2,12 +2,26 @@ package com.virspit.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VirspitGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VirspitGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route("path_route",  r-> r.path("/new")
+                        .filters(f -> f.addRequestHeader("Hello", "World")
+                                .rewritePath("/new", "/"))
+                        .uri("http://localhost:8083/"))
+                .build();
     }
 
 }
