9d703a3f605ac3b39986c50fb7dc1875133e698e
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-10 19:19:51 +0900

    <feat> auth temp 멤버 도메인 추가

--- virspit-auth/src/main/java/com/virspit/virspitauth/config/MongoDBConfig.java
+++ virspit-auth/src/main/java/com/virspit/virspitauth/config/MongoDBConfig.java
@@ -1,21 +1,21 @@
-package com.virspit.virspitauth.config;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-
-@Configuration
-public class MongoDBConfig implements InitializingBean {
-
-    @Autowired
-    @Lazy
-    private MappingMongoConverter mappingMongoConverter;
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
-    }
-}
+//package com.virspit.virspitauth.config;
+//
+//import org.springframework.beans.factory.InitializingBean;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Lazy;
+//import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+//import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+//
+//@Configuration
+//public class MongoDBConfig implements InitializingBean {
+//
+//    @Autowired
+//    @Lazy
+//    private MappingMongoConverter mappingMongoConverter;
+//
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+//    }
+//}
