7d5de8f6a2ac9d2982933acc00f34660f90995ff
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-11 21:42:57 +0900

    <feat> admin 프로젝트 생성

---
+++ virspit-admin/src/test/java/com/virspit/virspitadmin/VirspitAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virspit.virspitadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirspitAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
