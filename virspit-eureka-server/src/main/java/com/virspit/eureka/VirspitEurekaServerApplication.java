1f0b9b7bc9c2a4f5fe7d5975d4fb45f3764d6a40
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-13 03:49:39 +0900

    <feat> eureka server 생성

---
+++ virspit-eureka-server/src/main/java/com/virspit/eureka/VirspitEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.virspit.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirspitEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirspitEurekaServerApplication.class, args);
+    }
+
+}
