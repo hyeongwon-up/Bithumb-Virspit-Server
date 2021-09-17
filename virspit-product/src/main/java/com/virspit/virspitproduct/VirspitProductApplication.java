b355f21dc17f24249d8767a0c7ee1abea8c3c5d7
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-18 00:09:54 +0900

    <feat> eureka 및 swagger 적용

--- virspit-product/src/main/java/com/virspit/virspitproduct/VirspitProductApplication.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/VirspitProductApplication.java
@@ -2,10 +2,12 @@ package com.virspit.virspitproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VirspitProductApplication {
 
 	public static void main(String[] args) {
