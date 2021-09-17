913d28148b50b750521ad6d51443923ff9bdba41
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-17 12:52:58 +0900

    <feat> product 프로젝트 생성

---
+++ virspit-product/src/main/java/com/virspit/virspitproduct/VirspitProductApplication.java
@@ -0,0 +1,13 @@
+package com.virspit.virspitproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirspitProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirspitProductApplication.class, args);
+	}
+
+}
