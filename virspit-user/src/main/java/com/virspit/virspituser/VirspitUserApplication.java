b0d05c066daac4a0b15660bd5e1e0fd9fbb89204
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-20 03:12:59 +0900

    <feat> 유레카 client 추가

--- virspit-user/src/main/java/com/virspit/virspituser/VirspitUserApplication.java
+++ virspit-user/src/main/java/com/virspit/virspituser/VirspitUserApplication.java
@@ -2,8 +2,10 @@ package com.virspit.virspituser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VirspitUserApplication {
 
     public static void main(String[] args) {
