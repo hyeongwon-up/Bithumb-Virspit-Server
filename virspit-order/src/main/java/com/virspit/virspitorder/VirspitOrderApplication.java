e934a60735f2e328eb1fc3a5dd7075b546577f95
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-20 03:17:35 +0900

    virspit-order 프로젝트 생성

---
+++ virspit-order/src/main/java/com/virspit/virspitorder/VirspitOrderApplication.java
@@ -0,0 +1,13 @@
+package com.virspit.virspitorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirspitOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirspitOrderApplication.class, args);
+	}
+
+}
