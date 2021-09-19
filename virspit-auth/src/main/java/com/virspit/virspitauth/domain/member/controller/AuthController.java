25069a48a7d270fd5110183d2c23f11d0c6356c7
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-20 03:06:55 +0900

    <feat> 비밀번호 찾기 컨트롤러 추가

--- virspit-auth/src/main/java/com/virspit/virspitauth/domain/member/controller/AuthController.java
+++ virspit-auth/src/main/java/com/virspit/virspitauth/domain/member/controller/AuthController.java
@@ -26,19 +26,32 @@ public class AuthController {
     private final MemberService memberService;
 
     @PostMapping("/register")
+    @ApiOperation("회원가입")
     public ResponseEntity<String> addNewUser(@RequestBody MemberSignUpRequestDto memberSignUpRequestDto) {
         return ResponseEntity.ok(memberService.register(memberSignUpRequestDto));
     }
 
     @PostMapping("/login")
+    @ApiOperation("로그인")
     public MemberSignInResponseDto login(@RequestBody MemberSignInRequestDto memberSignInRequestDto) throws Exception {
         return memberService.login(memberSignInRequestDto);
     }
 
-    @GetMapping(path="/verify")
+    @GetMapping("/verify")
+    @ApiOperation("회원가입시 가입한 이메일 인증")
     public ResponseEntity<String> verifyEmail(@RequestParam("useremail") String userEmail, @RequestParam("key") String hash) {
-        log.info("verify controller 동작시작");
        return ResponseEntity.ok(memberService.verifyEmail(userEmail, hash));
+    }
+
+    @GetMapping("/findpwd")
+    @ApiOperation("비밀번호 찾기")
+    public ResponseEntity<String> findPassword(@RequestParam("useremail") String userEmail){
+        return ResponseEntity.ok(memberService.findPassword(userEmail));
+    }
+
+    @GetMapping("/verify/pwd")
+    public ResponseEntity<String> changePassword(@RequestParam("useremail") String userEmail, @RequestParam("key") String hash) {
+        return ResponseEntity.ok(memberService.changePassword(userEmail, hash));
     }
 
 
