5ace3361448f0f183d0c9c9dbf761e01a7fccef3
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-21 04:27:04 +0900

    <feat> TeamPlayerStoreRequestDto 생성자 추가

--- virspit-product/src/main/java/com/virspit/virspitproduct/domain/teamplayer/dto/request/TeamPlayerStoreRequestDto.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/domain/teamplayer/dto/request/TeamPlayerStoreRequestDto.java
@@ -4,6 +4,7 @@ import com.virspit.virspitproduct.domain.sports.entity.Sports;
 import com.virspit.virspitproduct.domain.teamplayer.entity.TeamPlayer;
 import com.virspit.virspitproduct.domain.teamplayer.entity.TeamPlayerType;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
@@ -34,6 +35,15 @@ public class TeamPlayerStoreRequestDto {
 
     @NotNull
     private Long sportsId;
+
+    @Builder
+    public TeamPlayerStoreRequestDto(String name, TeamPlayerType type, String description, Integer revenueShareRate, Long sportsId) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.revenueShareRate = revenueShareRate;
+        this.sportsId = sportsId;
+    }
 
     public TeamPlayer toTeamPlayer(final Sports sports) {
         return new TeamPlayer(name, type, description, revenueShareRate, sports);
