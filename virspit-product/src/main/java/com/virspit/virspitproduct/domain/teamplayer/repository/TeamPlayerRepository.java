4fa55da3dd98df1c2bad39a229d8843b9ad32537
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-20 14:22:29 +0900

    <feat> TeamPlayerRepository 추가

--- virspit-product/src/main/java/com/virspit/virspitproduct/domain/teamplayer/repository/TeamPlayerRepository.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/domain/teamplayer/repository/TeamPlayerRepository.java
@@ -2,8 +2,13 @@ package com.virspit.virspitproduct.domain.teamplayer.repository;
 
 import com.virspit.virspitproduct.domain.teamplayer.entity.TeamPlayer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TeamPlayerRepository extends JpaRepository<TeamPlayer, Long> {
+    @Query(value = "SELECT * FROM team_player WHERE name like %:name% AND sports_id = :sportsId ORDER BY name", nativeQuery = true)
+    List<TeamPlayer> findBySportsIdAndName(Long sportsId, String name);
 }
