a67a25583a08535839db840c62275aa2dc9e8cb6
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-20 14:22:07 +0900

    <feat> TeamPlayer 응답 dto 구현

--- virspit-product/src/main/java/com/virspit/virspitproduct/domain/teamplayer/dto/response/TeamPlayerResponseDto.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/domain/teamplayer/dto/response/TeamPlayerResponseDto.java
@@ -1,10 +1,36 @@
 package com.virspit.virspitproduct.domain.teamplayer.dto.response;
 
+import com.virspit.virspitproduct.domain.teamplayer.entity.TeamPlayer;
 import com.virspit.virspitproduct.domain.teamplayer.entity.TeamPlayerType;
+import io.swagger.models.auth.In;
+import lombok.Getter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
 public class TeamPlayerResponseDto {
-    private Long id;
-    private TeamPlayerType type;
-    private String description;
-    private float revenueShareRate;
+    private final Long id;
+    private final String name;
+    private final TeamPlayerType type;
+    private final String description;
+    private final Integer revenueShareRate;
+    private final Long sportsId;
+
+    private TeamPlayerResponseDto(final TeamPlayer teamPlayer) {
+        id = teamPlayer.getId();
+        name = teamPlayer.getName();
+        type = teamPlayer.getType();
+        description = teamPlayer.getDescription();
+        revenueShareRate = teamPlayer.getRevenueShareRate();
+        sportsId = teamPlayer.getSports().getId();
+    }
+
+    public static TeamPlayerResponseDto of(final TeamPlayer teamPlayer) {
+        return new TeamPlayerResponseDto(teamPlayer);
+    }
+
+    public static List<TeamPlayerResponseDto> of(final List<TeamPlayer> teamPlayers) {
+        return teamPlayers.stream().map(TeamPlayerResponseDto::new).collect(Collectors.toList());
+    }
 }
