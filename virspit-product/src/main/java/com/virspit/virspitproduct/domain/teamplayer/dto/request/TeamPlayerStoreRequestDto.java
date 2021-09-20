40ef07d583500a00e7732ade6308978e743c00b1
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-20 14:21:43 +0900

    <feat> TeamPlayer 저장 요청 dto 구현

--- virspit-product/src/main/java/com/virspit/virspitproduct/domain/teamplayer/dto/request/TeamPlayerStoreRequestDto.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/domain/teamplayer/dto/request/TeamPlayerStoreRequestDto.java
@@ -1,4 +1,41 @@
 package com.virspit.virspitproduct.domain.teamplayer.dto.request;
 
+import com.virspit.virspitproduct.domain.sports.entity.Sports;
+import com.virspit.virspitproduct.domain.teamplayer.entity.TeamPlayer;
+import com.virspit.virspitproduct.domain.teamplayer.entity.TeamPlayerType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TeamPlayerStoreRequestDto {
+    @NotBlank
+    @Length(max = 30)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private TeamPlayerType type;
+
+    @NotBlank
+    private String description;
+
+    @Min(0)
+    @Max(100)
+    private Integer revenueShareRate;
+
+    @NotNull
+    private Long sportsId;
+
+    public TeamPlayer toTeamPlayer(final Sports sports) {
+        return new TeamPlayer(name, type, description, revenueShareRate, sports);
+    }
 }
