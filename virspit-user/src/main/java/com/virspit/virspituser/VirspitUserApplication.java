9176661f7792a5696b6b9ddf6bf511ff0063b05f
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-22 02:06:57 +0900

    <feat> BaseTimeEntity 를 위한 EnableJpaAuditing 추가

--- virspit-user/src/main/java/com/virspit/virspituser/VirspitUserApplication.java
+++ virspit-user/src/main/java/com/virspit/virspituser/VirspitUserApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
 public class VirspitUserApplication {
 
     public static void main(String[] args) {
