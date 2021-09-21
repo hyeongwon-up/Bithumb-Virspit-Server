1fcf1dbdbb652248d19bb246fe449aeecdfb4e0b
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-21 21:55:48 +0900

    <feat> ProductRepository 추가

---
+++ virspit-product/src/main/java/com/virspit/virspitproduct/domain/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.virspit.virspitproduct.domain.product.repository;
+
+import com.virspit.virspitproduct.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
