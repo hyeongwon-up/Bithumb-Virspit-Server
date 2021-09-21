61356944d76cc627e248a1f4cf8e3c3af48ac135
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-21 18:27:44 +0900

    <chore> 미사용 import 제거

--- virspit-product/src/main/java/com/virspit/virspitproduct/domain/sports/entity/Sports.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/domain/sports/entity/Sports.java
@@ -7,7 +7,6 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
