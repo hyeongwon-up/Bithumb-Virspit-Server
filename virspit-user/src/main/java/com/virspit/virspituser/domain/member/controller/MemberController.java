36731202e6f31e4c77b6a6908b7f6b80a1158df8
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-22 00:13:09 +0900

    <feat> 비밀번호 변경 저장 컨트롤러 추가

--- virspit-user/src/main/java/com/virspit/virspituser/domain/member/controller/MemberController.java
+++ virspit-user/src/main/java/com/virspit/virspituser/domain/member/controller/MemberController.java
@@ -3,12 +3,9 @@ package com.virspit.virspituser.domain.member.controller;
 import com.virspit.virspituser.domain.member.dto.request.MemberSignUpRequestDto;
 import com.virspit.virspituser.domain.member.entity.Member;
 import com.virspit.virspituser.domain.member.service.MemberService;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,7 +19,7 @@ public class MemberController {
     @ApiOperation("회원가입 요청한 Member를 db에 저장")
     @PostMapping("/save")
     public String save(@RequestBody MemberSignUpRequestDto memberSignUpRequestDto) {
-        return memberService.save(memberSignUpRequestDto);
+        return memberService.registry(memberSignUpRequestDto);
     }
 
     @ApiOperation("email을 통해 member 정보 전달")
@@ -30,6 +27,12 @@ public class MemberController {
     public Member findByEmail(@RequestParam String memberEmail) {
         log.info("find email controller start");
         return memberService.findByEmail(memberEmail);
+    }
+
+    @ApiOperation("Member 비밀번호 변경 저장")
+    @PostMapping("/pwd")
+    public String changPwd(@RequestBody Member member) {
+        return memberService.changePwd(member);
     }
 
 
