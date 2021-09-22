81be43afddf0bb629201f5eefdcaebdcc9982e17
SONG22 <ansongi527@gmail.com>
2021-09-22 16:26:40 +0900

    <feat> nft 어드민에게 클레이 전송, 주문 시 토큰 생성 하는 method 구현

--- virspit-order/src/test/java/com/virspit/virspitorder/service/KasApiTest.java
+++ virspit-order/src/test/java/com/virspit/virspitorder/service/KasApiTest.java
@@ -7,11 +7,10 @@ import xyz.groundx.caver_ext_kas.CaverExtKAS;
 import xyz.groundx.caver_ext_kas.kas.tokenhistory.TokenHistoryQueryOptions;
 import xyz.groundx.caver_ext_kas.kas.wallet.WalletQueryOptions;
 import xyz.groundx.caver_ext_kas.rest_client.io.swagger.client.ApiException;
+import xyz.groundx.caver_ext_kas.rest_client.io.swagger.client.api.kip17.model.Kip17TokenListResponse;
 import xyz.groundx.caver_ext_kas.rest_client.io.swagger.client.api.kip17.model.Kip17TransactionStatusResponse;
 import xyz.groundx.caver_ext_kas.rest_client.io.swagger.client.api.tokenhistory.model.PageableNfts;
 import xyz.groundx.caver_ext_kas.rest_client.io.swagger.client.api.wallet.model.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Kas API 테스트")
 class KasApiTest {
@@ -61,28 +60,12 @@ class KasApiTest {
         System.out.println(transactionResult);
     }
 
-    @DisplayName("배포하는 계정이 수수료 부담")
-    @Test
-    void 스마트_컨트랙트_배포() throws ApiException {
-        // 배포할 스마트 컨트랙트의 바이트 코드
-        String input = "0x608060405234801561001057600080fd5b5061051f806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c8063693ec85e1461003b578063e942b5161461016f575b600080fd5b6100f46004803603602081101561005157600080fd5b810190808035906020019064010000000081111561006e57600080fd5b82018360208201111561008057600080fd5b803590602001918460018302840111640100000000831117156100a257600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600081840152601f19601f8201169050808301925050505050505091929192905050506102c1565b6040518080602001828103825283818151815260200191508051906020019080838360005b83811015610134578082015181840152602081019050610119565b50505050905090810190601f1680156101615780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b6102bf6004803603604081101561018557600080fd5b81019080803590602001906401000000008111156101a257600080fd5b8201836020820111156101b457600080fd5b803590602001918460018302840111640100000000831117156101d657600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600081840152601f19601f8201169050808301925050505050505091929192908035906020019064010000000081111561023957600080fd5b82018360208201111561024b57600080fd5b8035906020019184600183028401116401000000008311171561026d57600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600081840152601f19601f8201169050808301925050505050505091929192905050506103cc565b005b60606000826040518082805190602001908083835b602083106102f957805182526020820191506020810190506020830392506102d6565b6001836020036101000a03801982511681845116808217855250505050505090500191505090815260200160405180910390208054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156103c05780601f10610395576101008083540402835291602001916103c0565b820191906000526020600020905b8154815290600101906020018083116103a357829003601f168201915b50505050509050919050565b806000836040518082805190602001908083835b6020831061040357805182526020820191506020810190506020830392506103e0565b6001836020036101000a0380198251168184511680821785525050505050509050019150509081526020016040518091039020908051906020019061044992919061044e565b505050565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061048f57805160ff19168380011785556104bd565b828001600101855582156104bd579182015b828111156104bc5782518255916020019190600101906104a1565b5b5090506104ca91906104ce565b5090565b6104f091905b808211156104ec5760008160009055506001016104d4565b5090565b9056fea165627a7a723058203ffebc792829e0434ecc495da1b53d24399cd7fff506a4fd03589861843e14990029";
-
-        ContractDeployTransactionRequest request = new ContractDeployTransactionRequest();
-        request.setFrom("0x7ADe32dE57e41973EE1DCb036EFBa25F0FfedB29"); // 배포하는 계정 주소
-        request.setInput(input);
-        request.setGas(1500000L); // gas ?
-        request.submit(true);
-
-        TransactionResult transactionResult = caver.kas.wallet.requestSmartContractDeploy(request);
-        System.out.println(transactionResult);
-    }
-
     @DisplayName("컨트랙트의 모든 토큰 조회")
     @Test
     void nft_정보_조회() throws ApiException {
-        String contractAddress = "0xbbe63781168c9e67e7a8b112425aa84c479f39aa";
+        String contractAddress = "0xaa1453a5234b892284bd64aee484fb3f9cbf0fe9";
         TokenHistoryQueryOptions options = new TokenHistoryQueryOptions();
-        options.setSize((long)1);
+        options.setSize((long)10); // max size
 
         PageableNfts nfts = caver.kas.tokenHistory.getNFTList(contractAddress, options);
         System.out.println(nfts);
@@ -91,10 +74,12 @@ class KasApiTest {
     @DisplayName("KIP-17 컨트랙트 배포")
     @Test
     void 컨트랙트_배포() throws ApiException {
-        String name = "My First KIP-17";
+        String name = "My First KIP-17 test ";
         String symbol = "MFK";
         String alias = "my-first-kip17";
         Kip17TransactionStatusResponse res = caver.kas.kip17.deploy(name, symbol, alias);
+
+        System.out.println(res);
     }
 
 
@@ -102,15 +87,15 @@ class KasApiTest {
     @Test
     void 토큰_발행() throws ApiException {
         String contractAlias = "my-first-kip17";
-        String to = "0x{toAddress}";
-        String id = "0x1";
-        String uri = "https://link.to.your/token/metadata-0x1.json";
+        String to = "0x5C48744aC9500933c0168DBD9d4847Da1d3A3505";
+        String id = "0x2"; // randomTokenId
+        String uri = "https://metadata-store.klaytnapi.com/21d95c27-11ef-8140-64a8-19903ce3eebb/3b06f1b7-78fb-7a2c-032d-b91fb535f3a0.jpg";
 
         Kip17TransactionStatusResponse response = caver.kas.kip17.mint(contractAlias, to, id, uri);
         System.out.println(response);
     }
 
-    @DisplayName("KIP-17 토큰 전")
+    @DisplayName("KIP-17 토큰 전송")
     @Test
     void 토큰_전송() throws ApiException {
         String contractAlias = "my-first-kip17";
@@ -119,6 +104,27 @@ class KasApiTest {
         String tokenId = "0x321";
         Kip17TransactionStatusResponse response = caver.kas.kip17.transfer(contractAlias, ownerAddress, ownerAddress, recipientAddress, tokenId);
         System.out.println(response);
+    }
+
+    @DisplayName("GET /v1/contract/{alias-or-address}/token")
+    @Test
+    void 토큰_발행_목록_조회() throws ApiException {
+        // 1. 0x97ce7a4a281b66054cc5cbe3343c396487ac9ed3e237ef7afe70582b6a742030
+        // 2. 0x57859c7a25744780013d6fe0153d48b5b39333238e82b4e39e2a187bd909c696
+        Kip17TokenListResponse response = caver.kas.kip17.getTokenList("my-first-kip17");
+        System.out.println(response);
+    }
+
+    @DisplayName("트랜잭션 히스토리_유저")
+    @Test
+    void tokenHistory() throws ApiException {
+        String contractAddress = "0xaa1453a5234b892284bd64aee484fb3f9cbf0fe9";
+        String owner = "0x5C48744aC9500933c0168DBD9d4847Da1d3A3505";
+
+        PageableNfts nfts = caver.kas.tokenHistory.getNFTListByOwner(contractAddress, owner);
+        System.out.println(nfts);
+        int size = nfts.getItems().size();
+        System.out.println(size);
     }
 
 }
