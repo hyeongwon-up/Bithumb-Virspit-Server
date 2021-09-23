ba11ac3f4279acd65eb0b180c4a14a0651696e64
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-23 11:36:08 +0900

    <chore> feign client 활성화 annotation 추가

--- virspit-product/src/main/java/com/virspit/virspitproduct/VirspitProductApplication.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/VirspitProductApplication.java
@@ -2,9 +2,11 @@ package com.virspit.virspitproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 //@EnableDiscoveryClient
 public class VirspitProductApplication {
