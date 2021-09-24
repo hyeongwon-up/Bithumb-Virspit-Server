2697c6e1af2e497baad18b2f117f690486bd65aa
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-25 00:42:11 +0900

    <feat> createFavorite feat create

--- virspit-user/src/main/java/com/virspit/virspituser/domain/favorite/controller/FavoriteController.java
+++ virspit-user/src/main/java/com/virspit/virspituser/domain/favorite/controller/FavoriteController.java
@@ -1,13 +1,23 @@
 package com.virspit.virspituser.domain.favorite.controller;
 
 import com.virspit.virspituser.domain.favorite.service.FavoriteService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/favorite")
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
-    
+
+    @PostMapping("/{id}")
+    @ApiOperation("상품 좋아요하기")
+    public ResponseEntity<?> createFavorite(@PathVariable(name = "id") Long memberId, @RequestParam Long productId) {
+        return ResponseEntity.ok(favoriteService.createFavorite(memberId, productId));
+    }
+
+
 }
--- virspit-user/src/main/java/com/virspit/virspituser/domain/favorite/service/FavoriteService.java
+++ virspit-user/src/main/java/com/virspit/virspituser/domain/favorite/service/FavoriteService.java
@@ -1,7 +1,26 @@
 package com.virspit.virspituser.domain.favorite.service;
 
+import com.virspit.virspituser.domain.favorite.entity.Favorite;
+import com.virspit.virspituser.domain.favorite.repository.FavoriteRepository;
+import com.virspit.virspituser.domain.member.entity.Member;
+import com.virspit.virspituser.domain.member.repository.MemberRepository;
+import com.virspit.virspituser.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class FavoriteService {
+
+    private final FavoriteRepository favoriteRepository;
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+
+    public Favorite createFavorite(Long memberId, Long productId) {
+        Member member =  memberService.findById(memberId);
+        Favorite favorite = new Favorite(member, productId);
+        return favoriteRepository.save(favorite);
+    }
 }
