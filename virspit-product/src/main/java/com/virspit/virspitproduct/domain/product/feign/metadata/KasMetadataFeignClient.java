ba50d30c11592c80dfc92fcde0837a606ed67a48
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-27 16:48:29 +0900

    <chore> Metadata의 FeignClient annotation 속성 이름 변경

--- virspit-product/src/main/java/com/virspit/virspitproduct/domain/product/feign/metadata/KasMetadataFeignClient.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/domain/product/feign/metadata/KasMetadataFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "kas", url = "https://metadata-api.klaytnapi.com/v1/metadata", configuration = KasFeignConfig.class)
+@FeignClient(name = "metadata", url = "https://metadata-api.klaytnapi.com/v1/metadata", configuration = KasFeignConfig.class)
 public interface KasMetadataFeignClient {
     @PostMapping(value = "/asset", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     UploadAssetResponse uploadAsset(@RequestPart(value = "file") MultipartFile file);
