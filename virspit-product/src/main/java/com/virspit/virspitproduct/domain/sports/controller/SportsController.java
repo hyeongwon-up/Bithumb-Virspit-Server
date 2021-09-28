d9da764f787bc590a49cdda56cfedf497ad4ab07
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-29 04:11:39 +0900

    <chore> 컨트롤러의 반환 타입을 SuccessResponse로 변경

--- virspit-product/src/main/java/com/virspit/virspitproduct/domain/sports/controller/SportsController.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/domain/sports/controller/SportsController.java
@@ -1,10 +1,13 @@
 package com.virspit.virspitproduct.domain.sports.controller;
 
+import com.virspit.virspitproduct.domain.common.SuccessResponse;
 import com.virspit.virspitproduct.domain.sports.dto.request.SportsStoreRequestDto;
 import com.virspit.virspitproduct.domain.sports.dto.response.SportsResponseDto;
 import com.virspit.virspitproduct.domain.sports.service.SportsService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +16,6 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/sports")
@@ -21,28 +23,30 @@ public class SportsController {
     private final SportsService sportsService;
 
     @GetMapping
-    public List<SportsResponseDto> getAllSports() {
-        return sportsService.getAllSports();
+    public SuccessResponse<List<SportsResponseDto>> getAllSports(
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return SuccessResponse.of(sportsService.getAllSports(pageable));
     }
 
     @GetMapping("/{sportsId}")
-    public SportsResponseDto findSportsById(@PathVariable Long sportsId) {
-        return sportsService.findSportsById(sportsId);
+    public SuccessResponse<SportsResponseDto> findSportsById(@PathVariable Long sportsId) {
+        return SuccessResponse.of(sportsService.findSportsById(sportsId));
     }
 
-    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public SportsResponseDto addSports(@Valid @ModelAttribute SportsStoreRequestDto sportsCreateRequestDto) throws IOException {
-        return sportsService.addSports(sportsCreateRequestDto);
+    public SuccessResponse<SportsResponseDto> addSports(@Valid @ModelAttribute SportsStoreRequestDto sportsCreateRequestDto) throws IOException {
+        return SuccessResponse.of(sportsService.createSports(sportsCreateRequestDto), HttpStatus.CREATED);
     }
 
-    @PutMapping("/{sportsId}")
-    public SportsResponseDto updateSports(@PathVariable Long sportsId, @Valid @ModelAttribute SportsStoreRequestDto sportsCreateRequestDto) throws IOException {
-        return sportsService.updateSports(sportsId, sportsCreateRequestDto);
+    @PutMapping(value = "/{sportsId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public SportsResponseDto updateSports(@PathVariable Long sportsId, @Valid @ModelAttribute SportsStoreRequestDto sportsStoreRequestDto) throws IOException {
+        return sportsService.updateSports(sportsId, sportsStoreRequestDto);
     }
 
     @DeleteMapping("/{sportsId}")
-    public void deleteSports(@PathVariable Long sportsId) {
+    public SuccessResponse<String> deleteSports(@PathVariable Long sportsId) {
         sportsService.deleteSports(sportsId);
+        return SuccessResponse.of("deleted");
     }
 }
