f0730bf1c2f1e78da258769c39a4663a8cb99222
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-29 04:00:29 +0900

    <feat> Content Type 예외에 대한 핸들러 추가 및 메서드 중복 호출에 대한 변수화

--- virspit-product/src/main/java/com/virspit/virspitproduct/error/GlobalControllerExceptionHandler.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/error/GlobalControllerExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,45 +21,63 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
         log.error("handleMethodArgumentNotValidException", exception);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, exception.getBindingResult()), HttpStatus.BAD_REQUEST);
+        final ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+        return new ResponseEntity<>(
+                ErrorResponse.of(errorCode, exception.getBindingResult()),
+                HttpStatus.valueOf(errorCode.getStatus()));
     }
 
     // @ModelAttribute binding error
     @ExceptionHandler(BindException.class)
     protected ResponseEntity<ErrorResponse> handleBindException(BindException exception) {
         log.error("handleBindException", exception);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, exception.getBindingResult()), HttpStatus.BAD_REQUEST);
+        final ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+        return new ResponseEntity<>(
+                ErrorResponse.of(errorCode, exception.getBindingResult()),
+                HttpStatus.valueOf(errorCode.getStatus()));
     }
 
     // 404 Error
     @ExceptionHandler(NoHandlerFoundException.class)
     protected ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException exception) {
         log.error("handleNoHandlerFoundException", exception);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.NO_HANDLER_FOUND), HttpStatus.NOT_FOUND);
+        final ErrorCode errorCode = ErrorCode.NOT_FOUND;
+        return new ResponseEntity<>(ErrorResponse.of(errorCode), HttpStatus.valueOf(errorCode.getStatus()));
     }
 
+    // Type mismatch exception
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
         log.error("handleMethodArgumentTypeMismatchException", exception);
-        return new ResponseEntity<>(ErrorResponse.of(exception), HttpStatus.BAD_REQUEST);
+        final ErrorResponse errorResponse = ErrorResponse.of(exception);
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException exception) {
         log.error("handleHttpRequestMethodNotSupportedException", exception);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED), HttpStatus.METHOD_NOT_ALLOWED);
+        final ErrorCode errorCode = ErrorCode.METHOD_NOT_ALLOWED;
+        return new ResponseEntity<>(ErrorResponse.of(errorCode), HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException exception){
+        log.error("handleHttpMediaTypeNotSupportedException", exception);
+        final ErrorCode errorCode = ErrorCode.INVALID_CONTENT_TYPE;
+        return new ResponseEntity<>(ErrorResponse.of(errorCode), HttpStatus.valueOf(errorCode.getStatus()));
     }
 
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException exception) {
         log.error("handleBusinessException", exception);
         final ErrorCode errorCode = exception.getErrorCode();
-        return new ResponseEntity<>(ErrorResponse.of(errorCode), HttpStatus.valueOf(errorCode.getStatus()));
+        return new ResponseEntity<>(ErrorResponse.of(exception.getMessage(), errorCode), HttpStatus.valueOf(errorCode.getStatus()));
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception exception) {
         log.error("handleException", exception);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        return new ResponseEntity<>(ErrorResponse.of(errorCode), HttpStatus.valueOf(errorCode.getStatus()));
     }
 }
