a25c7e9e6cc16f9c129cb641f63519b2952cb493
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-29 04:01:02 +0900

    <chore> EntityNotFoundException 메시지 형식 변경

--- virspit-product/src/main/java/com/virspit/virspitproduct/error/exception/EntityNotFoundException.java
+++ virspit-product/src/main/java/com/virspit/virspitproduct/error/exception/EntityNotFoundException.java
@@ -5,7 +5,7 @@ import com.virspit.virspitproduct.error.ErrorCode;
 
 public class EntityNotFoundException extends BusinessException {
 
-    public EntityNotFoundException(String message) {
-        super(message, ErrorCode.ENTITY_NOT_FOUND);
+    public EntityNotFoundException(String target) {
+        super(target + ErrorCode.ENTITY_NOT_FOUND.getMessage(), ErrorCode.ENTITY_NOT_FOUND);
     }
 }
