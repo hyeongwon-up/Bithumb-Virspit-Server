fb2c2c30f6836627356013ebbabf57d1011c673b
Hyeongwon-up <leehwdatabase@gmail.com>
2021-10-04 18:34:17 +0900

    <feat> enableEureka, Enable Feign Client

--- virspit-gateway/src/main/java/com/virspit/gateway/VirspitGatewayApplication.java
+++ virspit-gateway/src/main/java/com/virspit/gateway/VirspitGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
