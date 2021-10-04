40fceb7746df8006cfdf34828215e4b569e192f8
Hyeongwon-up <leehwdatabase@gmail.com>
2021-10-05 02:44:15 +0900

    <test> auth Test class create

---
+++ virspit-auth/src/test/java/com/virspit/virspitauth/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package com.virspit.virspitauth.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void verifyUserEmail() {
+    }
+
+    @Test
+    void verifyNumber() {
+    }
+
+    @Test
+    void findPasssword() {
+    }
+
+    @Test
+    void getSHA512Token() {
+    }
+
+    @Test
+    void initPassword() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void checkFeign() {
+    }
+}
