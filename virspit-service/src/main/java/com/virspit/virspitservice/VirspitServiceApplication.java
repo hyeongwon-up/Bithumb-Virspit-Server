fee52e58234215b3affe071c9f8a787ef6e82159
Hyeongwon-up <leehwdatabase@gmail.com>
2021-10-06 02:42:15 +0900

    <feat> eureka setting

--- virspit-service/src/main/java/com/virspit/virspitservice/VirspitServiceApplication.java
+++ virspit-service/src/main/java/com/virspit/virspitservice/VirspitServiceApplication.java
@@ -2,8 +2,12 @@ package com.virspit.virspitservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class VirspitServiceApplication {
 
     public static void main(String[] args) {
