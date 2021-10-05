909f7b9b60a492f343b18df49c4e4321a8fe4d91
Hyeongwon-up <leehwdatabase@gmail.com>
2021-10-06 03:20:24 +0900

    <refac> path 변경

--- virspit-user/src/main/java/com/virspit/virspituser/domain/member/controller/MemberController.java
+++ virspit-user/src/main/java/com/virspit/virspituser/domain/member/controller/MemberController.java
@@ -29,21 +29,22 @@ public class MemberController {
 
     @ApiOperation("memberId로 member 찾기")
     @GetMapping("/{id}")
+    @Deprecated
     public Member findById(@PathVariable Long id) {
         return memberService.findById(id);
     }
 
     @ApiOperation("회원 정보 수정 (이름, 성별, 생년월일)")
     @PutMapping("/info/{id}")
-    public ResponseEntity<String> changeMemberInfo(@PathVariable("id") Long memberId,
+    public SuccessResponse<String> changeMemberInfo(@PathVariable("id") Long memberId,
                                                    @RequestBody MemberEditInfoRequestDto memberEditInfoRequestDto) {
-        return ResponseEntity.ok(memberService.changeMemberInfo(memberId, memberEditInfoRequestDto));
+        return SuccessResponse.of(memberService.changeMemberInfo(memberId, memberEditInfoRequestDto));
     }
 
     @ApiOperation("회원 비밀번호 변경")
     @PutMapping("/pwd")
-    public ResponseEntity<String> changePwd(@RequestBody MemberChangePwdRequestDto memberChangePwdRequestDto) {
-        return ResponseEntity.ok(memberService.changePwd(memberChangePwdRequestDto));
+    public SuccessResponse<String> changePwd(@RequestBody MemberChangePwdRequestDto memberChangePwdRequestDto) {
+        return SuccessResponse.of(memberService.changePwd(memberChangePwdRequestDto));
     }
 
 }
