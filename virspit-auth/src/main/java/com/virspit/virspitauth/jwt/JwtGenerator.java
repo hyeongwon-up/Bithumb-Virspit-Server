864b0dd9c60466c10af5fe16ce0a0895994b6f91
Hyeongwon-up <leehwdatabase@gmail.com>
2021-10-07 04:25:59 +0900

    <chore> 코드 정리

--- virspit-auth/src/main/java/com/virspit/virspitauth/jwt/JwtGenerator.java
+++ virspit-auth/src/main/java/com/virspit/virspitauth/jwt/JwtGenerator.java
@@ -20,6 +20,7 @@ public class JwtGenerator implements Serializable {
     public static final long JWT_REFRESH_TOKEN_VALIDITY = 24 * 60 * 60 * 7; //일주일
     @Value("${jwt.secret}")
     private String secret;
+
     //retrieve username from jwt token
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
@@ -29,10 +30,12 @@ public class JwtGenerator implements Serializable {
     public Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
+
     public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
+
     //for retrieveing any information from token we will need the secret key
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
@@ -57,10 +60,10 @@ public class JwtGenerator implements Serializable {
     public String generateAccessToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         List<String> li = new ArrayList<>();
-        for (GrantedAuthority a: userDetails.getAuthorities()) {
+        for (GrantedAuthority a : userDetails.getAuthorities()) {
             li.add(a.getAuthority());
         }
-        claims.put("role",li);
+        claims.put("role", li);
         return Jwts.builder().setClaims(claims).setSubject(userDetails.getUsername()).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_ACCESS_TOKEN_VALIDITY * 1000))
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
