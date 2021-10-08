c84326e279a553b3c8ec07df9d1d6c1b2264a686
Hyeongwon-up <leehwdatabase@gmail.com>
2021-10-08 19:51:05 +0900

    <feat> Token Controller 생성

---
+++ virspit-auth/src/main/java/com/virspit/virspitauth/controller/TokenController.java
@@ -0,0 +1,12 @@
+package com.virspit.virspitauth.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class TokenController {
+
+}
