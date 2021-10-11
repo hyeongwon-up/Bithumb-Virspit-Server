0bd863d3304660676ca0a55bfc67a30cf0643038
Hyeongwon-up <leehwdatabase@gmail.com>
2021-10-11 22:47:24 +0900

    <feat> 좋아요 중복 익셉션 추가

--- virspit-user/src/main/java/com/virspit/virspituser/domain/favorite/service/FavoriteService.java
+++ virspit-user/src/main/java/com/virspit/virspituser/domain/favorite/service/FavoriteService.java
@@ -6,6 +6,9 @@ import com.virspit.virspituser.domain.favorite.repository.FavoriteRepository;
 import com.virspit.virspituser.domain.member.entity.Member;
 import com.virspit.virspituser.domain.member.repository.MemberRepository;
 import com.virspit.virspituser.domain.member.service.MemberService;
+import com.virspit.virspituser.global.error.ErrorCode;
+import com.virspit.virspituser.global.error.exception.EntityNotFoundException;
+import com.virspit.virspituser.global.error.exception.InvalidValueException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,14 +25,21 @@ public class FavoriteService {
     private final MemberService memberService;
 
     public Favorite createFavorite(Long memberId, Long productId) {
-        Member member =  memberService.findById(memberId);
+        Member member = memberService.findById(memberId);
         Favorite favorite = new Favorite(member, productId);
-        return favoriteRepository.save(favorite);
+
+        if (favoriteRepository.findFavoriteByMemberIdAndProductId(memberId, productId).isEmpty()) {
+            return favoriteRepository.save(favorite);
+        } else {
+            throw new InvalidValueException(ErrorCode.FAVORITE_ALREADY_EXIST);
+        }
+
     }
 
     public String deleteFavorite(Long memberId, Long productId) {
         Favorite favorite = favoriteRepository
-                .findFavoriteByMemberIdAndProductId(memberId, productId);
+                .findFavoriteByMemberIdAndProductId(memberId, productId)
+                .orElseThrow(() -> new EntityNotFoundException(memberId.toString() + "이 좋아요 한 " + productId.toString()));
         favoriteRepository.delete(favorite);
         return "delete success";
     }
