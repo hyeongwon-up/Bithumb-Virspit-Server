296b4faddb55276b6d463f6fc3bd9b9f4178d013
Hyeongwon-up <leehwdatabase@gmail.com>
2021-10-11 21:38:43 +0900

    <feat> 에러처리 메세지 제공 추가

--- virspit-auth/src/main/java/com/virspit/virspitauth/error/exception/InvalidValueException.java
+++ virspit-auth/src/main/java/com/virspit/virspitauth/error/exception/InvalidValueException.java
@@ -8,6 +8,9 @@ public class InvalidValueException extends BusinessException {
     public InvalidValueException(String value) {
         super(value, ErrorCode.INVALID_INPUT_VALUE);
     }
+    public InvalidValueException(ErrorCode errorCode) {
+        super(errorCode);
+    }
 
     public InvalidValueException(String value, ErrorCode errorCode) {
         super(value, errorCode);
